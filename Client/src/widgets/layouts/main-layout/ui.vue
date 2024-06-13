eb5a8a5e48e13c47fd81a1b659f3df79d276c9a1
Edanriell <Edanriell@gmail.com>
2024-06-13 19:01:33 +0300

    finished main layout

--- Client/src/widgets/layouts/main-layout/ui.vue
+++ Client/src/widgets/layouts/main-layout/ui.vue
@@ -1,10 +1,19 @@
 <script lang="ts" setup></script>
 
 <template>
-	<main>
-		<h1>Main Layout</h1>
+	<main class="main">
+		<h1 class="visually-hidden">Interactive Rating Component</h1>
 		<slot />
 	</main>
 </template>
 
-<style scoped></style>
+<style scoped>
+	.main {
+		background: var(--very-dark-blue);
+		min-width: 100%;
+		min-height: 100vh;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+</style>
