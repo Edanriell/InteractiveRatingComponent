f97dad6e898c939274213ddac81b81cb251e9879
Edanriell <Edanriell@gmail.com>
2024-06-20 12:01:30 +0300

    instead implemented prive vues toasts

--- Client/src/app/main.ts
+++ Client/src/app/main.ts
@@ -3,9 +3,13 @@ import "./styles/styles.less";
 import { createApp } from "vue";
 import App from "./app.vue";
 import { router } from "./providers/router";
+import ToastService from "primevue/toastservice";
+import PrimeVue from "primevue/config";
 
 const app = createApp(App);
 
 app.use(router);
+app.use(PrimeVue, { ripple: true });
+app.use(ToastService);
 
 app.mount("#app");
